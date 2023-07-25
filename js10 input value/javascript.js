2022f2e4be85ae7b3320ac70edea1fa8033b5ebc
ThijmenHa <97098523@st.deltion.nl>
2023-07-25 13:16:03 +0200

    add

--- js10 input value/javascript.js
+++
@@ -1,3 +0,0 @@
-function functie(a) {
-
-}
--- js32 test/javascript.js
+++
@@ -1,9 +0,0 @@
-function onblurtest() {
-    console.log("De onblur werkt!")
-}
-
-function onkeyupevent() {
-    const waarde = document.getElementById("leeftijd").value;
-    const length = waarde.length;
-    document.getElementById("outputdiv").innerHTML = waarde + "<br>" + length
-}       
